4b71c8ee0cef589287e0e44120d21090dc3aae9e
yoshida0261 <yoshida261@gmail.com>
2019-02-19 01:11:49 +0900

    Initial commit

---
+++ app/src/main/java/jp/co/stah/actionbartest/SubActivity.kt
@@ -0,0 +1,12 @@
+package jp.co.stah.actionbartest
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class SubActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sub)
+    }
+}
