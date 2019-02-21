3d4d9cbf6f7e9c69fd7b8be3116d011efab4f9f4
yoshida0261 <yoshida261@gmail.com>
2019-02-22 01:50:04 +0900

    mockkサンプル１

--- app/src/test/java/jp/co/stah/actionbartest/ExampleUnitTest.kt
+++ app/src/test/java/jp/co/stah/actionbartest/ExampleUnitTest.kt
@@ -1,8 +1,9 @@
 package jp.co.stah.actionbartest
 
+import io.mockk.every
+import io.mockk.mockkObject
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -13,5 +14,23 @@ class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
+    }
+
+
+    object MockObj {
+        fun add(a: Int, b: Int) = a + b
+    }
+
+    // mockk
+    @Test
+    fun mockktest(){
+
+        mockkObject(MockObj) // aplies mocking to an Object
+
+        assertEquals(3, MockObj.add(1, 2))
+
+        every { MockObj.add(1, 2) } returns 55
+
+        assertEquals(55, MockObj.add(1, 2))
     }
 }
